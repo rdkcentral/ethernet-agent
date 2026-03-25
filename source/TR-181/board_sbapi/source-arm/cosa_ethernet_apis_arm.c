7aead07358f1a50da902e354e1e2e1dcae8e4964
Suriyanarayanan <224775237+suriya-prem@users.noreply.github.com>
2026-03-25 16:12:43 +0530

    Update cosa_ethernet_apis_arm.c

--- source/TR-181/board_sbapi/source-arm/cosa_ethernet_apis_arm.c
+++ source/TR-181/board_sbapi/source-arm/cosa_ethernet_apis_arm.c
@@ -697,11 +697,15 @@ CosaDmlEthPortGetClientMac
     }
 
 	CcspTraceDebug(("%s:total_eth_device      : %lu\n", __FUNCTION__, total_eth_device));
-    CcspTraceDebug(("%s: MAC Address : %02X:%02X:%02X:%02X:%02X:%02X, Port:%d, VLAN ID:%d, TX Rate:%d, RX Rate:%d, Active:%s\n", __FUNCTION__, 
-		   output_struct->eth_devMacAddress[0], output_struct->eth_devMacAddress[1], output_struct->eth_devMacAddress[2],
-           output_struct->eth_devMacAddress[3], output_struct->eth_devMacAddress[4], output_struct->eth_devMacAddress[5],
-		   output_struct->eth_port, output_struct->eth_vlanid, output_struct->eth_devTxRate, output_struct->eth_devRxRate,
-		   output_struct->eth_Active ? "TRUE" : "FALSE"));
+    for (int iLoopCount = 0; iLoopCount < (int) total_eth_device; iLoopCount++) {
+         CcspTraceInfo(("%s: MAC Address : %02X:%02X:%02X:%02X:%02X:%02X, Port:%d, VLAN ID:%d, TX Rate:%d, RX Rate:%d, Active:%s\n", __FUNCTION__,
+                   output_struct[iLoopCount].eth_devMacAddress[0], output_struct[iLoopCount].eth_devMacAddress[1],
+                   output_struct[iLoopCount].eth_devMacAddress[2], output_struct[iLoopCount].eth_devMacAddress[3],
+                   output_struct[iLoopCount].eth_devMacAddress[4], output_struct[iLoopCount].eth_devMacAddress[5],
+                   output_struct[iLoopCount].eth_port, output_struct[iLoopCount].eth_vlanid, output_struct[iLoopCount].eth_devTxRate,
+                   output_struct[iLoopCount].eth_devRxRate, output_struct[iLoopCount].eth_Active ? "TRUE" : "FALSE"));
+    }
+
    
         if ( total_eth_device )
         {
