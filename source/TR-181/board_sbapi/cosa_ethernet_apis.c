474318873c93444c8eecda2cfaa071ca30f9fe70
anatar818 <151121941+anatar818@users.noreply.github.com>
2026-06-19 15:40:32 +0530

    Update cosa_ethernet_apis.c

--- source/TR-181/board_sbapi/cosa_ethernet_apis.c
+++ source/TR-181/board_sbapi/cosa_ethernet_apis.c
@@ -3493,36 +3493,36 @@ CosaDmlEthInit(
 	   #endif
 
    #elif defined(_COSA_QCA_ARM_)
-      char wanPhyName[20] = {0},out_value[20] = {0};
+    char wanPhyName[20] = {0},out_value[20] = {0};
 
-      sysevent_get(sysevent_fd, sysevent_token, "wan_ifname", out_value, sizeof(out_value));
-	  if (out_value[0] != '\0')
-      {
-       strcpy(wanPhyName, out_value);
-      }
-      else
-      {
-       /* erouter0 interface should be available even WAN over LTE is active to make sure fallback to WANoE is working.
-        * RDKBACCL-896 */
-       strcpy(wanPhyName, "erouter0");
-      }
-      #ifdef CORE_NET_LIB
-      libnet_status status;
-      status=interface_down(ETHWAN_DEF_INTF_NAME);
-      if(status != CNL_STATUS_SUCCESS) 
-      {
-       CcspTraceInfo(("Failed to down the interface %s\n",ETHWAN_DEF_INTF_NAME));
-      }
-      status=interface_rename(ETHWAN_DEF_INTF_NAME,wanPhyName);
-      if(status != CNL_STATUS_SUCCESS) 
-      {
-       CcspTraceInfo(("Failed to rename the interface %s with %s\n",ETHWAN_DEF_INTF_NAME,wanPhyName));
-      }
-      status=interface_up(wanPhyName);
-      if(status != CNL_STATUS_SUCCESS) 
-      {
-       CcspTraceInfo(("Failed to up the interface %s\n",wanPhyName));
-      }
+    sysevent_get(sysevent_fd, sysevent_token, "wan_ifname", out_value, sizeof(out_value));
+	if (out_value[0] != '\0')
+    {
+      strcpy(wanPhyName, out_value);
+    }
+    else
+    {
+      /* erouter0 interface should be available even WAN over LTE is active to make sure fallback to WANoE is working.
+       * RDKBACCL-896 */
+      strcpy(wanPhyName, "erouter0");
+    }
+    #ifdef CORE_NET_LIB
+     libnet_status status;
+     status=interface_down(ETHWAN_DEF_INTF_NAME);
+     if(status != CNL_STATUS_SUCCESS) 
+     {
+      CcspTraceInfo(("Failed to down the interface %s\n",ETHWAN_DEF_INTF_NAME));
+     }
+     status=interface_rename(ETHWAN_DEF_INTF_NAME,wanPhyName);
+     if(status != CNL_STATUS_SUCCESS) 
+     {
+      CcspTraceInfo(("Failed to rename the interface %s with %s\n",ETHWAN_DEF_INTF_NAME,wanPhyName));
+     }
+     status=interface_up(wanPhyName);
+     if(status != CNL_STATUS_SUCCESS) 
+     {
+      CcspTraceInfo(("Failed to up the interface %s\n",wanPhyName));
+     }
     #else
     v_secure_system("ifconfig " ETHWAN_DEF_INTF_NAME" down");
     v_secure_system("ip link set "ETHWAN_DEF_INTF_NAME" name %s",wanPhyName);
@@ -3533,8 +3533,8 @@ CosaDmlEthInit(
     //Initialise ethsw-hal to get event notification from lower layer.
     if (CcspHalEthSwInit() != RETURN_OK)
     {
-       CcspTraceError(("Hal initialization failed \n"));
-       return ANSC_STATUS_FAILURE;
+      CcspTraceError(("Hal initialization failed \n"));
+      return ANSC_STATUS_FAILURE;
     }
 #endif
 #if defined (FEATURE_RDKB_WAN_AGENT)
