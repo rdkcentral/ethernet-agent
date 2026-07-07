79be529300d2bd8ab455a9cbd39975c828437f8c
anatar818 <151121941+anatar818@users.noreply.github.com>
2026-07-07 12:39:11 +0530

    Update cosa_ethernet_apis.c

--- source/TR-181/board_sbapi/cosa_ethernet_apis.c
+++ source/TR-181/board_sbapi/cosa_ethernet_apis.c
@@ -3463,20 +3463,55 @@ CosaDmlEthInit(
 #else
       #if (defined(_PLATFORM_RASPBERRYPI_) || defined(_PLATFORM_TURRIS_) || defined(_PLATFORM_BANANAPI_R4_)) && !defined(FEATURE_RDKB_VLAN_MANAGER)
 
-	   char wanPhyName[20] = {0},out_value[20] = {0};
+	   char wanPhyName[20] = {0},out_value[20] = {0},fileValue[20] = {0};;
+       FILE *fp = NULL;
        macaddr_t macAddr;
        char wan_mac[18];
        if (!syscfg_get(NULL, "wan_physical_ifname", out_value, sizeof(out_value)))
        {
-          strncpy(wanPhyName, out_value, sizeof(wanPhyName) - 1);
-       }
-       else
-       {
-          /* erouter0 interface should be available even WAN over LTE is active to make sure fallback to WANoE is working.
-         * RDKBACCL-896 */
+         fp = fopen("/nvram/wan_name.txt", "r");
+         if (fp == NULL)
+         {
+           fp = fopen("/nvram/wan_name.txt", "w");
+           if (fp == NULL)
+              strcpy(wanPhyName, "erouter0");
+           else
+           {
+              fprintf(fp, "%s", out_value);
+               strncpy(wanPhyName, out_value, sizeof(wanPhyName) - 1);
+               fclose(fp);
+           }
+         }
+         else if (fgets(fileValue, sizeof(fileValue), fp) == NULL)
+         {
+             fclose(fp);
+             fp = fopen("/nvram/wan_name.txt", "w");
+             if (fp != NULL)
+             {
+               fprintf(fp, "%s", out_value);
+               strncpy(wanPhyName, out_value, sizeof(wanPhyName) - 1);
+               fclose(fp);
+             }
+             else
+                strcpy(wanPhyName, "erouter0");
+         }
+         else
+         {
+             fclose(fp);
+             fileValue[strcspn(fileValue, "\r\n")] = '\0';
+             if (strcmp(fileValue, out_value) != 0)
+             {
+               if ( syscfg_set_commit( NULL,  "wan_physical_ifname", fileValue ) != 0 )
+                    strcpy(wanPhyName, "erouter0");
+               else
+                strncpy(wanPhyName,fileValue, sizeof(wanPhyName) - 1);
+             }
+             else
+                 strncpy(wanPhyName, out_value, sizeof(wanPhyName) - 1);
+         }
+      }
+      else
           strcpy(wanPhyName, "erouter0");
-       }
-
        memset(&macAddr,0,sizeof(macaddr_t));
        getInterfaceMacAddress(&macAddr,wanPhyName);
        memset(wan_mac,0,sizeof(wan_mac));
